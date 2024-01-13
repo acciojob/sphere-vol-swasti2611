bc79177314e18f9059a682733e3096177869f4fc
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-13 17:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius=document.getElementById("radius"). value;
+	let volume=document.getElementById("volume")
+	let result=(4/3)*(3.14)*radius*radius*radius;
+	volume.value=result;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
